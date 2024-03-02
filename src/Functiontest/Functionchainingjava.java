f98bfa7234681535f87f240186112bea4beba528
Pacificgroups <Prashantranjan532@gmail.com>
2024-03-03 00:15:55 +0530

    ghgjbhn

---
+++ src/Functiontest/Functionchainingjava.java
@@ -0,0 +1,15 @@
+package Functiontest;
+
+
+import java.util.function.Function;
+
+public class Functionchainingjava {
+    public static void main(String[] args) {
+
+        Function<Integer,Integer> li=(s)-> s*2;
+        Function<Integer,Integer> li2=(g)-> g*g;
+
+        System.out.println(li2.andThen(li).apply(4));
+        System.out.println(li.compose(li2).apply(4));
+    }
+}
